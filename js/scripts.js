bac4ffe71e18d2fc8a4ea07502b1bf950f08f3f0
LeiKD <dkomen180@gmail.com>
2020-02-28 20:52:52 +0300

    First part of the button function

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,20 @@ function order() {
   } else if (number<1 || number>10) {
     alert("Please enter a number between 1 and 10 pizzas")
   } else if (size==="large"){
-    var s= parseInt("1000")
+    var s= parseInt(1000)
   } else if (size==="medium") {
-    var s= parseInt("800")
+    var s= parseInt(800)
   } else if (size==="small") {
-    var s= parseInt("550")
+    var s= parseInt(550)
   } else if (topping==="bacon"||topping==="cheese") {
-    var t= parseInt("100")
+    var t= parseInt(100)
   } else if (x==="yes") {
     var d= parseInt("100")
   } else {
-    var s=parseInt("0")
-    var t=parseInt("50")
-    var d=parseInt("0")
+    var s=parseInt(0)
+    var t=parseInt(50)
+    var d=parseInt(0)
   }
   var price= number*(s+t+d)
-  alert("Your price plus delivery is" + "Ksh."+price+"/=." + "Thank you for using Kapizza services.")
+  alert("Your price plus delivery is" + "Ksh."+ price +"/=." + "Thank you for using Kapizza services.")
 }
