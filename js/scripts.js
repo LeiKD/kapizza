52121bd08a2f16a5438b966f5e76d5ae5e21ff14
LeiKD <dkomen180@gmail.com>
2020-02-29 01:53:44 +0300

    Edited the prompt to allow input of adress

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,10 @@ function what() {
   document.getElementById("x").innerHTML= n + x
   document.getElementById("y").innerHTML= y
   document.getElementById("z").innerHTML= z
-  if (n<1||n>10) {
+  if (n<1 || n>10) {
     alert("Please input the number of pizzas you want between 1 and 10!")
   } else if (d==="Delivery to your door for Ksh 100/=") {
-    var where=prompt("Please input your location here...")
+    var where= prompt("Please input your street adress here...")
     alert("Your package will be promptly delivered at " + where + " promptly. Thank you for using Kapizza services!")
   } else {
     alert("Please  make sure you pick-up your package at  our premises at no delivery cost. Thank you!")
