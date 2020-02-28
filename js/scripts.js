3f7c76b176784d0f32963309dedcb9583acc082b
LeiKD <dkomen180@gmail.com>
2020-02-29 01:24:55 +0300

    Add alert statements concerning deliveries

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,17 @@ function what() {
   var y=document.getElementById("crust").value
   var z=document.getElementById("toppings").value
   var n=document.getElementById("number").value
+  var d=document.getElementById("delivery").value
   document.getElementById("x").innerHTML= n + x
   document.getElementById("y").innerHTML= y
-  document.getElementById("z").innerHTML=z
+  document.getElementById("z").innerHTML= z
   if (n<1||n>10) {
     alert("Please input the number of pizzas you want between 1 and 10!")
+  } else if (d==="Delivery to your door for Ksh 100/=") {
+    var where=prompt("Please input your location here...")
+    alert("Your package will be promptly delivered at " + where + " promptly. Thank you for using Kapizza services!")
   } else {
+    alert("Please  make sure you pick-up your package at  our premises at no delivery cost. Thank you!")
   }
 }
 $(document).ready(function() {
