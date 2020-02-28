367340759097ed18c3f0dd6201ccb9a89cc894dd
LeiKD <dkomen180@gmail.com>
2020-02-29 01:07:43 +0300

    Edited the summary of order to accomodate number

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,15 @@
-// var size= document.getElementById("size").value;
-// var crust= document.getElementById("crust").value;
-// var topping= document.getElementById("toppings").value;
-// var number= parseInt(document.getElementById("number").value);
-// var x= document.getElementById("delivery").value
-// if (size==="large") {
-//   var s1= "A large-sized pizza at Ksh 1000/="
-// } else if (size==="medium") {
-//   var s1= "A medium-sized pizza at Ksh 800/="
-// } else {
-//   var s1="A small-sized pizza at Ksh 550/="
-// }
-// function order() {
-//   if (number==="") {
-//     alert("Please make the necessary selections!")
-//   } else if (number<1 || number>10) {
-//     alert("Please enter a number between 1 and 10 pizzas")
-//   } else if (size==="large"){
-//     var s= parseInt(1000)
-//   } else if (size==="medium") {
-//     var s= parseInt(800)
-//   } else if (size==="small") {
-//     var s= parseInt(550)
-//   } else if (topping==="bacon"||topping==="cheese") {
-//     var t= parseInt(100)
-//   } else if (x==="yes") {
-//     var d= parseInt("100")
-//   } else {
-//     var s=parseInt(0)
-//     var t=parseInt(50)
-//     var d=parseInt(0)
-//   }
-//   var price= number*(s+t+d)
-//   alert("Your price plus delivery is" + "Ksh."+ price +"/=." + "Thank you for using Kapizza services.")
-// }
-
 function what() {
   var x=document.getElementById("size").value 
   var y=document.getElementById("crust").value
   var z=document.getElementById("toppings").value
-  document.getElementById("x").innerHTML= x
+  var n=document.getElementById("number").value
+  document.getElementById("x").innerHTML= n + x
   document.getElementById("y").innerHTML= y
   document.getElementById("z").innerHTML=z
-
+  if (n<1||n>10) {
+    alert("Please input the number of pizzas you want between 1 and 10!")
+  } else {
+  }
 }
 $(document).ready(function() {
   $(".place").click(function() {
