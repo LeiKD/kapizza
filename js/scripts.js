c83592af3b0fc74b5689fd126e508d21e235bbf3
LeiKD <dkomen180@gmail.com>
2020-02-29 20:18:42 +0300

    Edit the summary to isolate the order details

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
 
 function what() {
-  var x=document.getElementById("size").value 
-  var y=document.getElementById("crust").value
-  var z=document.getElementById("toppings").value
-  var n=document.getElementById("number").value
+  var x=parseInt(document.getElementById("size").value)
+  var y=parseInt(document.getElementById("crust").value)
+  var z=parseInt(document.getElementById("toppings").value)
+  var n=parseInt(document.getElementById("number").value)
   var d=document.getElementById("delivery").value
-  document.getElementById("x").innerHTML= n + x
-  document.getElementById("y").innerHTML= y
-  document.getElementById("z").innerHTML= z
+  if (x===1000) {
+    document.getElementById("x").innerHTML= n + " large-sized pizza(s) at Ksh 1000/= each"
+  } else if (x===800) {
+    document.getElementById("x").innerHTML= n + " medium-sized pizza(s) at Ksh 800/= each"
+  } else if (x===550) {
+    document.getElementById("x").innerHTML= n + " regular-sized pizza(s) at Ksh 550/= each"
+  } else if (y===100) {
+    document.getElementById("y").innerHTML= "With crispy crust(s) at Ksh 100/= each"
+  } else if (y===80) {
+    document.getElementById("y").innerHTML= "With stuffed crust(s) at Ksh 80/= each"
+  } else if (y===60) {
+    document.getElementById("y").innerHTML= "With gluten-free crust(s) at Ksh 60/= each"
+  } else if (z===50) {
+    document.getElementById("z").innerHTML= "With pepperoni topping(s) at Ksh 50/= each"
+  } else if (z===80) {
+    document.getElementById("z").innerHTML= "With mushroom topping(s) at Ksh 80/= each"
+  } else if (z===100) {
+    document.getElementById("z").innerHTML= "With cheese topping(s) at Ksh 100/= each"
+  }
   if (n<1 || n>10) {
     alert("Please input the number of pizzas you want between 1 and 10!")
   } else if (d==="Delivery to your door for Ksh 100/=") {
