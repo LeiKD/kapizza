5ab330d67d792045df8e0a11e8ae33eeff38f009
LeiKD <dkomen180@gmail.com>
2020-03-01 11:49:38 +0300

    Add shake effect to how

--- js/scripts.js
+++ js/scripts.js
@@ -31,7 +31,7 @@ function what() {
   if (d===100) {
     document.getElementById("d").innerHTML= "Delivery to your doorstep at Ksh 100/="
     var where= prompt("Please input your street adress here...")
-    alert("We have received your order. Your package will be promptly delivered at " + where + " promptly. Thank you for using Kapizza services!")
+    alert("We have received your order. Your package will be promptly delivered at " + "'" + where + "'" + " promptly. Thank you for using Kapizza services!")
   } else {
     alert("We have received your order. Please  make sure you pick-up your package at  our premises at no delivery cost. Thank you!")
   }
